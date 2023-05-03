aefa324f2fc64346a2ed103637d31f41382c8e02
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2023-05-03 13:31:42 +0600

    create component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <div className="text-center text-bg-primary p-4">
+            <h1 className="">Sofiullah Iqbal Kiron</h1>
+        </div>
+    );
+}
